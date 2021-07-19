232efc729ac072850fcf5ee09ed36ac28cf5caa0
Claudia Ibarra <Smaragdus7@users.noreply.github.com>
2021-07-19 12:43:11 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { Auth0Provider } from "@auth0/auth0-react";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Auth0Provider domain="dev-3mga0ky3.us.auth0.com" clientId="OsjaQrBhRUSXn2FMua3vTHXy0Eq2jh3i" redirectUri="window.location.origin"><App /></Auth0Provider>
+    <Auth0Provider domain="your_domain" clientId="your_client_id" redirectUri="window.location.origin"><App /></Auth0Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
